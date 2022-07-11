663af4b7b822d722081ff8624b6bae9c37bc23f6
awg333 <awg333@naver.com>
2022-07-11 13:40:31 +0900

    app.py 업데이트

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from pymongo import MongoClient
+import certifi
+import jwt
+import datetime
+import hashlib
+app = Flask(__name__)
+
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://team5:sparta@cluster0.odclv.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
+db = client['yogi-eoddae']
+
+print(db)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
