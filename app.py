06927c68d86327f73dc9eb5c239bc6b1f19631be
awg333 <awg333@naver.com>
2022-07-11 19:46:43 +0900

    index 시작으로 업데이트

--- app.py
+++ app.py
@@ -18,14 +18,18 @@ SECRET_KEY = 'TEAMFIVE'
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
+    curstatus = 0
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
-        return render_template('index.html', status = 1)
+        curstatus = 1
     except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        curstatus = 0
+        #return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        curstatus = 0
+        #return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+    return render_template('index.html', status=curstatus)
 
 @app.route('/login')
 def login():
@@ -36,6 +40,14 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/posting')
+def posting():
+    return render_template('posting.html')
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
 
 # [안웅기] 로그인을 위한 API
 
@@ -88,13 +100,6 @@ def api_login():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
-@app.route('/posting')
-def posting():
-    return render_template('posting.html')
-
-@app.route('/detail')
-def detail():
-    return render_template('detail.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
--- static/js/header.js
+++ static/js/header.js
@@ -1,16 +1,5 @@
 function logout(){
     $.removeCookie('mytoken');
     alert('로그아웃!')
-    window.location.href='/login'
-}
-
-function login(){
-    console.log('login')
-    window.location.href='/login'
-}
-
-function signup(){
-    console.log('signup')
-    window.location.href='/register'
-
+    window.location.href='/'
 }
