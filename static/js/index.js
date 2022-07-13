56e64f1814acae0ddc8c35cc9a41b6df3cd23af5
mgyang53 <mg960503@gmail.com>
2022-07-13 19:48:54 +0900

    메인페이지 등장모션 추가

--- static/js/index.js
+++ static/js/index.js
@@ -2,3 +2,11 @@ function post_detail(id){
 
     window.location.href=`/detail/${id}`
 }
+
+$(document).ready(function () {
+    $('.intro_wrap').addClass('show');
+
+    setTimeout(function () {
+        $('.intro_wrap').addClass('hide');
+    }, 2000);
+})
