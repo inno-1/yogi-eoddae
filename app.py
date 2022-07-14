9734a6d089e83df8d7e6927bea42a0a104ed53c1
awg333 <awg333@naver.com>
2022-07-14 09:44:16 +0900

    포트번호 수정

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def home():
     cur_status, cur_id = token_request()
     order = request.args.get('order_by')
     result = {'status': cur_status}
-    print(order)
+
     if order == None:
         order = 'new'
 
@@ -496,4 +496,4 @@ def post_edit():
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
